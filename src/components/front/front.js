8884ee8e89c8d4937b2ff8798cf3cedd9fbfe991
Navaneet1999 <93372592+Navaneet1999@users.noreply.github.com>
2022-07-13 00:00:19 +0530

    basic styles added

--- src/components/front/front.js
+++ src/components/front/front.js
@@ -3,17 +3,20 @@ import "./front.css"
 export const Front = () => {
     return(
         <div>
-            <nav>
-                <div>
+            <nav className="navbar">
+                <div className="nav_comp_img">
+                    <img src="https://mma.prnewswire.com/media/1631809/Masai_Logo.jpg?p=facebook" alt=""/>
+                </div>
+                <div className="nav_comp">
                     <a href="Lectures">Lectures</a>
                 </div>
-                <div>
+                <div className="nav_comp">
                     <a href="Assignment">Assignment</a>
                 </div>
-                <div>
+                <div className="nav_comp">
                     <a href="Admin">Admin</a>
                 </div>
-                <div>
+                <div className="nav_comp">
                     <a href="Login">Login</a>
                 </div>
             </nav>
