bb5347dd3f37564c92927352f965cc23c4de7dd0
Navaneet1999 <93372592+Navaneet1999@users.noreply.github.com>
2022-07-14 01:11:13 +0530

    login added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Front } from './components/front/front';
+import { Login } from './components/login/login';
 
 function App() {
   return (
     <div className="App">
       <Front/>
+      <Login/>
     </div>
   );
 }
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,9 +1,10 @@
+import BasicTextFields from "./form/form"
 import "./login.css"
 
 export const Login = () => {
     return(
         <div>
-            
+            <BasicTextFields/>
         </div>
     )
 }
